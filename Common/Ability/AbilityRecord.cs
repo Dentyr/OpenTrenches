7e73561a1a00bcb349a3b3579e75236b4145717b
Justin Sun <jsun103@uw.edu>
2026-02-06 10:06:49 -0800

    Made ability immutable after creation

--- Common/Ability/AbilityRecord.cs
+++ Common/Ability/AbilityRecord.cs
@@ -5,12 +5,12 @@ namespace OpenTrenches.Common.Ability;
 
 public record class AbilityRecord
 {
-    public required int ID;
-    public required string Name;
-    public required string Description;
-    public float DefenseMod = 0;
-    public required float Cooldown;
-    public float Duration = 0;
+    public required int ID { get; init; }
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+    public float DefenseMod{ get; init; } = 0;
+    public required float Cooldown { get; init; }
+    public float Duration{ get; init; } = 0;
 
     public Texture2D Thumbnail = TextureLibrary2D.NotFound;
     
