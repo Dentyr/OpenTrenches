51bc42fd5266f9ca29d99d7d28039e3d75da9931
Justin Sun <jsun103@uw.edu>
2026-04-04 17:10:07 -0700

    wall tileset

--- Common/Resources/TileSetLibrary.cs
+++ Common/Resources/TileSetLibrary.cs
@@ -3,5 +3,6 @@ using System;
 
 public partial class TileSetLibrary : Node
 {
-    public static TileSet GrassTileSet { get; } = ResourceLoader.Load<TileSet>("Common/Resources/TileSet/GrassTileSet.tres");
+    public static readonly TileSet GrassTileSet = ResourceLoader.Load<TileSet>("Common/Resources/TileSet/GrassTileSet.tres");
+    public static readonly TileSet WallTileSet  = ResourceLoader.Load<TileSet>("Common/Resources/TileSet/WallTileSet.tres");
 }
