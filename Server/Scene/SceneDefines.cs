526015f7236400b42a23d3444f30ea01caad8db7
Justin Sun <jsun103@uw.edu>
2026-04-05 13:20:42 -0700

    updated physics layers

--- Server/Scene/SceneDefines.cs
+++ Server/Scene/SceneDefines.cs
@@ -9,12 +9,32 @@ public static class SceneDefines
         public const uint NilLayer = 0;
         public const uint BulletLayer = (uint)1 << 0;
         public const uint CharacterLayer = (uint)1 << 1;
-        public const uint TerrainLayer = (uint)1 << 2;
+        /// <summary>
+        /// Collision layer for trench tiles
+        /// </summary>
+        public const uint TrenchTileLayer = (uint)1 << 4;
+        /// <summary>
+        /// Collision layer for ground tiles
+        /// </summary>
+        public const uint GroundTileLayer = (uint)1 << 5;
+        /// <summary>
+        /// Collision layer for objects inside a trench
+        /// </summary>
+        public const uint TrenchObjectLayer = (uint)1 << 8;
+        /// <summary>
+        /// Collision layer for objects on the ground
+        /// </summary>
+        public const uint GroundObjectLayer = (uint)1 << 9;
+        /// <summary>
+        /// Inaccessible area
+        /// </summary>
+        public const uint BarrierLayer = (uint)1 << 31;
+        
 
 
         public const uint AllMask = 0xFFFFFFFF;
 
-        public const uint BulletMask = CharacterLayer | TerrainLayer;
+        public const uint BulletMask = CharacterLayer | GroundTileLayer;
 
     }
 
