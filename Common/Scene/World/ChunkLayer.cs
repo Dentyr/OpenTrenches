a2a7c9d05f4f60f418fd75aa2468d06c22cf02b6
Justin Sun <jsun103@uw.edu>
2026-04-06 15:34:43 -0700

    show terrain tiles

--- Common/Scene/World/ChunkLayer.cs
+++ Common/Scene/World/ChunkLayer.cs
@@ -4,13 +4,16 @@ using Godot;
 using OpenTrenches.Common.Collections;
 using OpenTrenches.Common.Contracts.Defines;
 using OpenTrenches.Common.Contracts.DTO.UpdateModel;
+using OpenTrenches.Common.Resources;
 using OpenTrenches.Common.World;
 
 namespace OpenTrenches.Common.Scene.World;
 
 public partial class ChunkLayer : Node2D
 {
-    private readonly TileMapLayer TerrainLayer;
+    private readonly TileMapLayer GrassLayer;
+    private readonly TileMapLayer TrenchLayer;
+
     private readonly TileMapLayer WallLayer;
 
     protected ChunkArray2D Source { get; }
@@ -22,11 +25,16 @@ public partial class ChunkLayer : Node2D
 
 
         // set chunks 
-        TerrainLayer = new()
+        TrenchLayer = new()
+        {
+            TileSet = TileSetLibrary.TrenchTileSet,
+        };
+        AddChild(TrenchLayer);
+        GrassLayer = new()
         {
             TileSet = TileSetLibrary.GrassTileSet,
         };
-        AddChild(TerrainLayer);
+        AddChild(GrassLayer);
 
         WallLayer = new()
         {
@@ -34,7 +42,46 @@ public partial class ChunkLayer : Node2D
         };
         AddChild(WallLayer);
 
-        // Load each chunk into tilemaplayer
+
+        // Initializing defaults
+
+        int cellSizeX = ChunkGrid.SizeX * CommonDefines.ChunkSize;
+        int cellSizeY = ChunkGrid.SizeY * CommonDefines.ChunkSize;
+
+        // Initializing terrain
+
+
+        for (int x = 0; x < cellSizeX; x ++)
+        {
+            for (int y = 0; y < cellSizeY; y ++)
+            {
+                Vector2I cell = new(x, y);
+                GrassLayer.SetCell(cell, TileSetDefines.DefualtTerrainAtlasID, TileSetDefines.FillAtlasPosition);
+                TrenchLayer.SetCell(cell, TileSetDefines.MiscCellAltasID, TileSetDefines.MiscClearedPosition);
+            }
+            
+        }
+
+        //Initizing border
+        List<Vector2I> border = new((cellSizeX + 2) * (cellSizeY + 2));
+        for (int x = -1; x < cellSizeX + 1; x ++)
+        {
+            border.Add(new(x, -1));
+            border.Add(new(x, cellSizeY));
+        }
+        for (int y = -1; y < cellSizeY + 1; y ++)
+        {
+            border.Add(new(-1, y));
+            border.Add(new(cellSizeX, y));
+        }
+        WallLayer.SetCellsTerrainConnect([..border], 0, 0);
+        GrassLayer.SetCellsTerrainConnect([..border], 0, 0);
+        TrenchLayer.SetCellsTerrainConnect([..border], 0, 1);
+
+
+
+
+        // Load chunk states
         for (int x = 0; x < ChunkGrid.SizeX; x ++) 
         {
             for (int y = 0; y < ChunkGrid.SizeY; y ++) 
@@ -42,21 +89,6 @@ public partial class ChunkLayer : Node2D
                 Initialize(x, y, ChunkGrid[x, y]);
             }
         }
-
-        int sizeX = ChunkGrid.SizeX * CommonDefines.ChunkSize;
-        int sizeY = ChunkGrid.SizeY * CommonDefines.ChunkSize;
-        List<Vector2I> border = new((sizeX + 2) * (sizeY + 2));
-        for (int x = -1; x < sizeX + 1; x ++)
-        {
-            border.Add(new(x, -1));
-            border.Add(new(x, sizeY));
-        }
-        for (int y = -1; y < sizeY * CommonDefines.ChunkSize + 1; y ++)
-        {
-            border.Add(new(-1, y));
-            border.Add(new(sizeX, y));
-        }
-        WallLayer.SetCellsTerrainConnect([..border], 0, 0);
     }
 
     private void Initialize(int x, int y, IChunk chunk)
@@ -67,33 +99,34 @@ public partial class ChunkLayer : Node2D
 
     private void UpdateTile(Tile? tile, int x, int y)
     {
-        // terrain
-        TerrainLayer.SetCellsTerrainConnect([new(x, y)], 0, GetTerrainFromTile(tile?.Type));
+        SetTerrain([new(x, y)], tile?.Type);
     }
 
-    private void LoadChunk(int x, int y, IChunk chunk)
+    private void LoadChunk(int chunkx, int chunky, IChunk chunk)
     {
-        int xoffset = x * CommonDefines.ChunkSize;
-        int yoffset = y * CommonDefines.ChunkSize;
+
+        int xoffset = chunkx * CommonDefines.ChunkSize;
+        int yoffset = chunky * CommonDefines.ChunkSize;
 
         // Batch terrain setting for efficiency
-        List<Vector2I>[] Terrains = [[], []];
+        List<Vector2I> TrenchTerrain = [];
         for (int cellx = 0; cellx < CommonDefines.ChunkSize; cellx ++)
         {
+            int x = xoffset + cellx;
             for (int celly = 0; celly < CommonDefines.ChunkSize; celly ++)
             {
+                int y = yoffset + celly;
+
+
                 // For each cell in the chunk, add it to its respective terrain list
-                Vector2I position = new(xoffset + cellx, yoffset + celly);
-                int terrainSet = GetTerrainFromTile(chunk[x, y]?.Type);
-                Terrains[terrainSet].Add(position);
+                if (chunk[cellx, celly]?.Type == TileType.Trench)
+                {
+                    TrenchTerrain.Add(new(x, y));
+                }
             }
         }
-        // for each terrain list, set cells to respective terrain type.
-        for (int terrainset = 0; terrainset < Terrains.Length; terrainset ++)
-        {
-            if (Terrains[terrainset].Count < 1) continue; // skip if empty
-            TerrainLayer.SetCellsTerrainConnect([..Terrains[terrainset]], 0, terrainset);
-        }
+        SetTerrain([..TrenchTerrain], TileType.Trench);
+        // GrassLayer.SetCellsTerrainConnect([..Terrains[terrainset]], 0, terrainset);
     }
     /// <summary>
     /// Returns the tile map layer index associated with the tile type.
@@ -103,9 +136,25 @@ public partial class ChunkLayer : Node2D
     {
         return type switch
         {
-            TileType.Trench => 0,
+            TileType.Trench => 1,
             _ => 0,
         };
+    }
+
+    private void SetTerrain(Godot.Collections.Array<Vector2I> points, TileType? type)
+    {
+        switch (type)
+        {
+            case TileType.Trench:
+                TrenchLayer.SetCellsTerrainConnect(points, 0, 0);
+                GrassLayer.SetCellsTerrainConnect(points, 0, -1);
+                break;
+            case TileType.Clear:
+            case null:
+                TrenchLayer.SetCellsTerrainConnect(points, 0, 1);
+                GrassLayer.SetCellsTerrainConnect(points, 0, 0);
+                break;
+        }
     }
 
 
