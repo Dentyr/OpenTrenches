7fbddb6da12c204c5613a3229600b7b567ca9875
Justin Sun <jsun103@uw.edu>
2026-04-06 15:33:28 -0700

    trench tileset

---
+++ Common/Resources/TileSetDefines.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+namespace OpenTrenches.Common.Resources;
+
+public static class TileSetDefines
+{
+    public static int DefualtTerrainAtlasID = 1;
+    /// <summary>
+    /// ID for atlases that contain misc cells for tile sets, such as empty tiles or default tiles.
+    /// </summary>
+    public static int MiscCellAltasID = 101;
+
+
+    /// <summary>
+    /// The atlas position of the tile that is continugous (set, and surrounded on all sides by similarly set tiles)
+    /// </summary>
+    /// <returns></returns>
+    public static Vector2I FillAtlasPosition = new(1, 1);
+
+    /// <summary>
+    /// The atlas position of the tile that is meant for unset tiles
+    /// </summary>
+    public static Vector2I MiscClearedPosition = new(0, 0);
+}
--- Common/Resources/TileSetLibrary.cs
+++ Common/Resources/TileSetLibrary.cs
@@ -1,8 +1,9 @@
 using Godot;
 using System;
 
-public partial class TileSetLibrary : Node
+public static class TileSetLibrary
 {
+    public static readonly TileSet TrenchTileSet = ResourceLoader.Load<TileSet>("Common/Resources/TileSet/TrenchTileSet.tres");
     public static readonly TileSet GrassTileSet = ResourceLoader.Load<TileSet>("Common/Resources/TileSet/GrassTileSet.tres");
     public static readonly TileSet WallTileSet  = ResourceLoader.Load<TileSet>("Common/Resources/TileSet/WallTileSet.tres");
 }
