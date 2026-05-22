8d89a2f183f41ce3c134ca5859f7577bae4ed744
Justin Sun <jsun103@uw.edu>
2026-05-21 17:21:01 -0700

    characters run to trench

--- Server/Scripting/Player/Agent/HoldTask.cs
+++ Server/Scripting/Player/Agent/HoldTask.cs
@@ -2,6 +2,7 @@ using System;
 using Godot;
 using OpenTrenches.Common.Contracts.Defines;
 using OpenTrenches.Common.World;
+using OpenTrenches.Core.Scripting.World;
 using OpenTrenches.Server.Scripting.World;
 
 namespace OpenTrenches.Server.Scripting.Player.Agent;
@@ -11,7 +12,16 @@ namespace OpenTrenches.Server.Scripting.Player.Agent;
 /// </summary>
 public class HoldTask : AbstractAgentTask
 {
+    /// <summary>
+    /// Chance per reason tick to find another random place to secure
+    /// </summary>
+    private const float ChangeToSeekNewSecureLocation = 0.1f;
+
     private const float DefaultRange = 5f;
+    /// <summary>
+    /// When the agent has determined a location to secure, look this number of cells away to find the ideal position
+    /// </summary>
+    private const int SeekPositionRange = 2;
 
     /// <summary>
     /// Moves to secure the area around this position and seeks a good place to entrench
@@ -24,6 +34,11 @@ public class HoldTask : AbstractAgentTask
 
     /// <summary>
     /// The specific position to secure within the area
+    /// </summary>
+    private Vector2I _secureTarget;
+
+    /// <summary>
+    /// The exact position to move to
     /// </summary>
     private Vector2 _targetPosition;
 
@@ -42,17 +57,65 @@ public class HoldTask : AbstractAgentTask
         TargetArea = position;
         _range = range;
 
+        _secureTarget = SeekSecureTarget();
+        _targetPosition = _secureTarget.CellToPosition();
+    }
+
+    /// <summary>
+    /// Looks for a random securable target location
+    /// </summary>
+    private Vector2I SeekSecureTarget()
+    {
         float angle = GD.Randf() * Mathf.Tau;
         float radius = Mathf.Sqrt(GD.Randf()) * _range;
-
-        _targetPosition = TargetArea + new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * radius;
+        return (Vector2I)(TargetArea + new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * radius);
     }
+
+    /// <summary>
+    /// Searches for the exact position to move to in the target location, looking for the nearest entrenched position
+    /// </summary>
+    /// <returns></returns>
+    private Vector2 SeekPositionTarget(IWorld2DQueryService queryService, IServerChunkArray chunks)
+    {
+        Vector2I best = _secureTarget;
+        // score for the current best position. lower is better
+        float bestScore = SeekPositionRange * SeekPositionRange + 10;
+
+        int startX = Math.Max(0, _secureTarget.X - SeekPositionRange);
+        int endX = Math.Min(chunks.SizeX, _secureTarget.X + SeekPositionRange + 1);
+
+        int startY = Math.Max(0, _secureTarget.Y - SeekPositionRange);
+        int endY = Math.Min(chunks.SizeY, _secureTarget.Y + SeekPositionRange + 1);
+
+        for (int x = startX; x < endX; x ++)
+        {
+            for (int y = startY; y < endY; y ++)
+            {
+                if (chunks[x, y] == TileType.Trench)
+                {
+                    Vector2I cell = new(x, y);
+                    float score = _secureTarget.DistanceSquaredTo(cell);
+                    if (score < bestScore)
+                    {
+                        bestScore = score;
+                        best = cell;
+                    }
+                }
+            }
+        }
+        return best.CellToPosition();
+    }
+
+
 
     /// <remarks>
     /// Hold tasks are held indefinitely until changed from above
     /// </remarks>
     public override bool Reason(Character character, IWorld2DQueryService queryService, IServerChunkArray chunks)
     {
+        if (GD.Randf() < ChangeToSeekNewSecureLocation)
+            _secureTarget = SeekSecureTarget();
+        _targetPosition = SeekPositionTarget(queryService, chunks);
         Positioned = TaskServices.Navigate(
             character, _targetPosition, queryService, 
             error: 1
